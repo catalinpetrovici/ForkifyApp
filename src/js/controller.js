b7ee98155339598d59d8f3966ec5540a202e1ac7
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-04-01 22:34:52 +0300

    Implementing Search Results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,8 +1,9 @@
 import icons from 'url:../img/icons.svg';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import recipeView from './views/recipeView';
 import * as model from './model';
+import recipeView from './views/recipeView';
+import searchView from './views/searchView';
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -26,8 +27,25 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    // 1. Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2. Load search results
+    await model.loadSearchResults(query);
+
+    // 3. Render restults
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerrender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -1,13 +1,18 @@
+import { async } from 'regenerator-runtime';
 import { API_URL } from './config.js';
 import { getJSON } from './views/helpers.js';
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     const { recipe } = data.data;
 
@@ -23,6 +28,25 @@ export const loadRecipe = async function (id) {
     };
   } catch (err) {
     // Temp error handling
+    console.error(`${err} 💥💥💥`);
+    throw err;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+
+    state.search.results = data.data.recipes.map((rec) => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+  } catch {
     console.error(`${err} 💥💥💥`);
     throw err;
   }
