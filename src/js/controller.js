b5a587e84e0bf519cb89079f199d5fba11f74315
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-04-02 21:15:53 +0300

    Storing Bookmarks With localStorage

--- src/js/controller.js
+++ src/js/controller.js
@@ -25,6 +25,8 @@ const controlRecipes = async function () {
 
     // 0. Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+
+    // 3. Updating bookmarks view
     bookmarksView.update(model.state.bookmarks);
 
     // 1. Loading recipe
@@ -34,7 +36,7 @@ const controlRecipes = async function () {
     recipeView.render(model.state.recipe);
   } catch (err) {
     // alert(`Controller: ${err.message}, ${err}`);
-    console.log(err);
+    console.error(err);
     recipeView.renderError();
   }
 };
@@ -90,7 +92,12 @@ const controlAddBookmark = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerrender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServing);
   recipeView.addHandlerAddBookmark(controlAddBookmark);
