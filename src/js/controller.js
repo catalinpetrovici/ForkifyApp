52b9ac7addb6762dfd85a515fc4f520c85d722ba
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-04-02 20:54:02 +0300

    Implementing Bookmarks

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,6 +6,7 @@ import recipeView from './views/recipeView';
 import searchView from './views/searchView';
 import resultsView from './views/resultsView';
 import paginationView from './views/paginationView';
+import bookmarksView from './views/bookmarksView';
 
 // if (module.hot) {
 //   module.hot.accept();
@@ -24,6 +25,7 @@ const controlRecipes = async function () {
 
     // 0. Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // 1. Loading recipe
     await model.loadRecipe(id);
@@ -77,9 +79,15 @@ const controlServing = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  // 1. Add/remove bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.deleteBookmark(model.state.recipe.id);
+
+  // 2. Update recipe view
   recipeView.update(model.state.recipe);
+
+  // 3. Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
